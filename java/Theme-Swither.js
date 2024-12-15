ff58363210b747411b160a363ca7314a0abc181d
Braconi1 <elvir.pandur@stu.ibu.edu.ba>
2024-12-15 22:30:06 +0100

    faq and interactive gallery

--- java/Theme-Swither.js
+++ java/Theme-Swither.js
@@ -3,6 +3,17 @@ document.body.classList.add(currentTheme);
 
   
 function toggleTheme() {
+    var moonIcon = document.getElementById('moon-icon');
+    var sunIcon = document.getElementById('sun-icon');
+  
+    if (moonIcon.style.display === "none") {
+    moonIcon.style.display = "inline";
+    sunIcon.style.display = "none";
+    } else {
+    moonIcon.style.display = "none";
+    sunIcon.style.display = "inline";
+    }
+    
     let theme = document.body.classList.contains('dark') ? 'light' : 'dark';
     document.body.classList.remove('light', 'dark');
     document.body.classList.add(theme);
